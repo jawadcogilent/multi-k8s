b665b383ed9ef04f3ae13aa9b8fca0ba10994f54
Ahmad Jawad <ahmad@jawad.lan>
2019-11-20 20:16:57 +0500

    Deployment with version compatibility resolved

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-docker build -t jawadcogilent/multi-client -f ./client/Dockerfile ./client
-docker build -t jawadcogilent/multi-server -f ./server/Dockerfile ./server
-docker build -t jawadcogilent/multi-worker -f ./worker/Dockerfile ./worker
+docker build -t jawadcogilent/multi-client:latest -t jawadcogilent/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jawadcogilent/multi-server:latest -t jawadcogilent/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jawadcogilent/multi-worker:latest -t jawadcogilent/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jawadcogilent/multi-client
-docker push jawadcogilent/multi-server
-docker push jawadcogilent/multi-worker
+docker push jawadcogilent/multi-client:latest
+docker push jawadcogilent/multi-server:latest
+docker push jawadcogilent/multi-worker:latest
+
+docker push jawadcogilent/multi-client:$SHA
+docker push jawadcogilent/multi-server:$SHA
+docker push jawadcogilent/multi-worker:$SHA
 
 kubectl apply -f k8s
-#kubectl set image deployments/server-deployment server=jawadcogilent/multi-server
-kubectl rollout restart deployment deployment/server-deployment
-kubectl rollout restart deployment deployment/client-deployment
-kubectl rollout restart deployment deployment/worker-deployment
+
+kubectl set image deployments/client-deployment server=jawadcogilent/multi-client:$SHA
+kubectl set image deployments/server-deployment server=jawadcogilent/multi-server:$SHA
+kubectl set image deployments/worker-deployment server=jawadcogilent/multi-worker:$SHA
+
+## Uncomment the below lines only if you use kubectl version 1.16 or above
+#kubectl rollout restart deployment deployment/server-deployment
+#kubectl rollout restart deployment deployment/client-deployment
+#kubectl rollout restart deployment deployment/worker-deployment
